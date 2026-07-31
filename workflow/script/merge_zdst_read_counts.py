9033382cdcb4e30714e43eb8c32b1a2c3bb9152e
tdayris <thibault.dayris@gustaveroussy.fr>
2026-07-31 11:03:06 +0200

    fix: bamcount format

--- workflow/script/merge_zdst_read_counts.py
+++ workflow/script/merge_zdst_read_counts.py
@@ -4,6 +4,7 @@
 
 import argparse
 import collections.abc
+import io
 import multiprocessing
 import os
 import pathlib
@@ -11,62 +12,73 @@ import polars
 import pretty_errors
 import rich_argparse
 import session_info
-import xopen
+import zstandard
 
 from loguru import logger
 
-
-def load_zstd_table(path: pathlib.Path, enumeration: str = "") -> polars.DataFrame:
+def load_zstd_table(path: pathlib.Path, enumeration: str = "", schema: dict[str, polars.PolarsDataType], keep_gene_names: bool = False, suffix: str = ".all.tsv.zst") -> polars.DataFrame:
     """
     Load a zdst table, keep the counts column (the one with the highest counts)
     and rename it with sample name.
     """
     logger.info(f"{enumeration} Loading {str(path.resolve())}")
-    sample_name: str = str(path.name)[: -len(".zdst")]
+    sample_name: str = str(path.name)[: -len(suffix)]
     abs_path: str = str(path.resolve())
 
-    # Get header names and prepare best casting type
-    with xopen.xopen(abs_path, "rt") as f:
-        header_line = f.readline()
-    header = header_line.strip().split("\t")
-
-    index_col: str = header[0]
-    int_cols: list[str] = header[1:]
-
-    schema: dict[str, polars.PolarsDataType] = {index_col: polars.Utf8}
-    for col in int_cols:
-        schema[col] = polars.UInt32
-
     # Load the table
-    with xopen.xopen(abs_path, "rt") as f:
-        df = polars.read_csv(f, schema=schema, low_memory=True)
+    with open(abs_path, "rb") as compressed_file_stream:
+        with zstandard.ZstdDecompressor().stream_reader(compressed_file_stream) as uncompressed_stream:
+            table_stream = io.TextIOWrapper(uncompressed_stream)
+            df: polars.DataFrame = polars.read_csv(
+                uncompressed_stream,
+                separator="\t",
+                schema=schema, 
+                low_memory=True, 
+                has_header=False,
+            )
 
     # Find counts column
-    sums: dict[str, int] = df.select(polars.col(int_cols).sum()).row(0, named=True)
-    max_sum: int = max(sums.values())
-    max_col: str = max_cols[0]
+    sums: dict[str, int] = df.select(polars.col(list(schema.keys())[1:]).sum()).row(0, named=True)
+    max_col: str = max(sums.keys(), key=sums.get)
     logger.debug(f"Keeping {max_col} from {sample_name}")
 
     # Select index and that column, renaming the integer column
-    return df.select(polars.col(index_col), polars.col(max_col).alias(sample_name))
+    if keep_gene_names:
+        df = df.select(polars.col(list(schema.keys())[0]), polars.col(max_col).alias(sample_name))
+    else:
+        df = df.select(polars.col(max_col).alias(sample_name))
+
+    logger.debug(df.head())
+    return df
 
 
-def load_parallel(paths: list[pahtlib.Path], cores: int = 1) -> list[polars.DataFrame]:
+def load_parallel(paths: list[pathlib.Path], cores: int = 1) -> list[polars.DataFrame]:
     """Load multiple files in parallel"""
-    ctx = mp.get_context("spawn")
+    load_function = {
+        "bamcount": load_zstd_bamcount,
+        "feature_count": load_zstd_featurecount,
+    }
+    ctx = multiprocessing.get_context("spawn")
     with ctx.Pool(processes=cores) as pool:
-        return pool.map(load_zstd_table, paths)
+        nb_path: int = len(paths)
+        enum_paths = [(p, f"{i+1}/{nb_path}") for i, p in enumerate(paths)]
+        return pool.starmap(load_zstd_bamcount, enum_paths)
 
 
 def search_zstd_tables(
     path: pathlib.Path,
+    source_programm: str = "feature_count",
 ) -> collections.abc.Generator[pathlib.Path, None, None]:
     """Recursively search zsdt tables and yeidls them"""
-    logger.debug(f"Searching tables into {str(path.resolve())}")
+    logger.debug(f"Searching {source_programm} tables into {str(path.resolve())}")
+    extensions: dict[str, str] = {
+        "feature_count": tuple([".gene_fc_count_all.tsv.zst"]),
+        "bamcount": tuple([".all.tsv.zst"]),
+    }
     for child in path.iterdir():
-        if child.is_directory():
-            yield from search_zstd_tables(child)
-        elif child.is_file() and str(child.name).endswith("_fc_count_all.tsv.zst"):
+        if child.is_dir():
+            yield from search_zstd_tables(child, source_programm)
+        elif child.is_file() and str(child.name).endswith(extensions[source_programm]):
             yield child
 
 
@@ -77,26 +89,64 @@ def main() -> None:
     )
 
     parser.add_argument(
-        "input_directory",
+        "-i", "--input_directory",
         help="Path to input directory",
         default=os.getcwd(),
         type=str,
     )
     parser.add_argument(
-        "output_table",
+        "-o", "--output_table",
         help="Path to gzipped output table",
         default=f"{os.getcwd()}/Counts.csv.gz",
         type=str,
     )
-    args = parser.parse_args()
-    # List of zstd-compressed CSV files
-    paths: list[pathlib.Path] = list(
-        search_zstd_tables(path=pathlib.Path(args.input_directory))
+    parser.add_argument(
+        "-s", "--source",
+        help="Source programm used to produce tables",
+        default="bamcount",
+        choices={"feature_count", "bamcount"},
+        type=str,
     )
-    logger.debug(f"Found {len(dfs)} tables to process.")
+    parser.add_argument(
+        "-t", "--threads",
+        help="Maximum number of threads used",
+        default=multiprocessing.cpu_count(),
+        type=int,
+    )
+    parser.add_argument(
+        "-f", "--force",
+        help="Overwrite existing result file",
+        action="store_true",
+    )
+    parser.add_argument(
+        "-v", "--verbose",
+        help="Increase verbosity",
+        action="store_true",
+    )
+    args = parser.parse_args()
+    logger.info("Command line parsed")
+    # Check existing output table
+    output_table: str = str(pathlib.Path(args.output_table))
+    if not args.force and pathlib.Path(args.output_table).exists():
+        raise FileExistsError(
+            f"{output_table} already exists. "
+            "Remove it, or use `-f|--force` argument"
+        )
+
+    # List of zstd-compressed CSV files
+    logger.info(f"Looking for {args.source} tables in {args.input_directory}")
+    paths: list[pathlib.Path] = list(
+        search_zstd_tables(
+            path=pathlib.Path(args.input_directory),
+            source_programm=args.source,
+        ),
+    )
+    logger.debug(f"Found {len(paths)} tables to process.")
 
     # Load all tables, it should take roughly 140Mb per table.
-    dfs: list[polars.DataFrame] = load_parallel(paths=paths)
+    dfs: list[polars.DataFrame] = [load_zstd_bamcount(paths[0], enumeration=f"1/{len(paths)}", keep_gene_names=True,)]
+    #dfs: list[polars.DataFrame] = load_parallel(paths=paths, cores=args.threads)
+    dfs: list[polars.DataFrame] = dfs + [load_zstd_bamcount(p, "") for p in paths[1:]]
     logger.debug("Data loaded")
 
     # Merge side-by-side (same row count, same order, different column names)
@@ -105,7 +155,6 @@ def main() -> None:
     logger.info("Data merged")
 
     # Save gzipped results
-    output_table: str = str(pathlib.Path(args.output_table))
     merged.write_csv(
         output_table,
         compression="gzip",
